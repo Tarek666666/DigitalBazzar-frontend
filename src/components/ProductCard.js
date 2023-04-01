5af1be6a2c92b1e450734274b5ce6bf3c6ed78ab
Tarek <2b3zab666@gmail.com>
2023-04-01 21:18:50 +0200

    fixed form validation to accept only email format

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,6 +10,22 @@ function ProductCard({ product }) {
     const [alert , setAlert] = useState(false);
     const [message , setMessage] = useState('');
 
+    const card = document.querySelector('.one-product-container');
+    card.addEventListener('mousemove', (e) => {
+      const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+      const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+      card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+    });
+    
+    card.addEventListener('mouseenter', (e) => {
+      card.style.transition = 'none';
+    });
+    
+    card.addEventListener('mouseleave', (e) => {
+      card.style.transition = 'all 0.5s ease';
+      card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    });
+
     const  changeAddToCartBtn = () => {
         setAlert(true)
         setMessage('Product Added')
