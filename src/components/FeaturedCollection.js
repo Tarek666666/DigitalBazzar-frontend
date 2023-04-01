0442876245efab840616fcc8917125f311a63b75
Tarek <2b3zab666@gmail.com>
2023-04-01 21:15:48 +0200

    fixed form validation to accept only email format

--- src/components/FeaturedCollection.js
+++ src/components/FeaturedCollection.js
@@ -4,6 +4,22 @@ import ProductCard from '../components/ProductCard';
 // componenet to be as the featured products displayed on home page , admin can update it
 function FeaturedCollection({featuredProds , isDarkMode}) {
 
+    const card = document.querySelector('.one-product-container');
+card.addEventListener('mousemove', (e) => {
+  const xAxis = (window.innerWidth / 2 - e.pageX) / 25;
+  const yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+});
+
+card.addEventListener('mouseenter', (e) => {
+  card.style.transition = 'none';
+});
+
+card.addEventListener('mouseleave', (e) => {
+  card.style.transition = 'all 0.5s ease';
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+});
+
   return (
         <div className={!isDarkMode ? 'featured-collection-conitaner-dark py-5 hidden-responsive' :'featured-collection-conitaner py-5 hidden-responsive'}>
                 <div className='row d-flex justify-content-center'>
